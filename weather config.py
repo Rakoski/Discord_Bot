575cfb19d5b5c7a1384c749d56a75b4902631bef
Mateus Stainer Rakoski <126481665+Rakoski@users.noreply.github.com>
2023-04-18 10:01:40 -0300

    Atualizar o weather config.py

--- weather config.py
+++ weather config.py
@@ -2,7 +2,7 @@
 from pyowm.owm import OWM
 owm = OWM('015f8141edc79dda4164c30a8929fcc6')
 mgr = owm.weather_manager()
-observation = mgr.weather_at_place('Paranavaí,BR')  # the observation object is a box containing a weather object
+observation = mgr.weather_at_place('London ,UK')  # the observation object is a box containing a weather object
 weather = observation.weather    # short version of status (eg. 'Rain')
  # detailed version of status (eg. 'light rain')
 print(f"{weather.status}, {weather.detailed_status}")
