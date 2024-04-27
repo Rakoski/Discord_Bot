82544cfed079cb6022dd884fef138d39eb5acd46
Mateus Rakoski <126481665+Rakoski@users.noreply.github.com>
2024-04-27 16:00:40 -0300

    Atualizar o weather config.py

--- weather config.py
+++ weather config.py
@@ -2,7 +2,7 @@
 from pyowm.owm import OWM
 owm = OWM('015f8141edc79dda4164c30a8929fcc6')
 mgr = owm.weather_manager()
-observation = mgr.weather_at_place('London ,UK')
+observation = mgr.weather_at_place('London, UK')
 weather = observation.weather of status 
 print(f"{weather.status}, {weather.detailed_status}")
 temp_dict_celsius = weather.temperature('celsius')
